79c52bd97e2785d197b5c32c85a55507d83a9a77
lyf85 <lyf8511@outlook.com>
2024-12-17 19:53:37 +0800

    1

--- P编程-P1-基础部分/04操作列表/01遍历列表.py
+++ P编程-P1-基础部分/04操作列表/01遍历列表.py
@@ -2,3 +2,10 @@ magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
     print(magician)
     print(f"goog!{magician.title()},very good!\n")
+print('thinks!')
+
+foods = ['board', 'cake', 'rice', 'dumpling']
+for food in foods:
+    print(food.upper())
+    print(f'I like {food.title}!\n')
+print('haha')
