e0dab4bb9943b16280d813e4873dc0ac79b0c829
lyf85 <lyf8511@outlook.com>
2024-12-20 21:01:39 +0800

    4

--- P编程-P1-基础部分/04操作列表/04使用列表的一部分.py
+++ P编程-P1-基础部分/04操作列表/04使用列表的一部分.py
@@ -1,5 +1,6 @@
-cubes = []
-for number in range(1,10):
-    cube = number **3
-    cubes.append(cube)
-print(cubes)
+players = ['111', '222', '333', '444', '555']
+print(players[1:3])
+print(players[:3])
+print(players[-3:])
+print(players[:-3])
+print(players[1:5:2]) # 其中2是步长，注意切片之间是冒号，不仅位置，也包括步长之间
