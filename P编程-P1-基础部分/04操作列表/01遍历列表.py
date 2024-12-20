a3d07a8d2481c055902c4d420f665b3978cc7aef
lyf85 <lyf8511@outlook.com>
2024-12-20 20:48:04 +0800

    4

--- P编程-P1-基础部分/04操作列表/01遍历列表.py
+++ P编程-P1-基础部分/04操作列表/01遍历列表.py
@@ -22,6 +22,18 @@ for value in range(1,11):
     squares.append(square)
 print(squares)
 
+# 更简化的代码
+squares = []
 for value in range(1,11):
     squares.append(value ** 2)
 print(squares)
+
+# 更更简化的代码——列表推导式
+squares = [value**2 for value in range(1,11)]
+print(squares)
+
+# 对数值列表执行简单的统计计算
+digits = [1,2,3,4,5,6,7,8,9,0]
+print(max(digits))
+print(min(digits))
+print(sum(digits))
---
+++ P编程-P1-基础部分/04操作列表/03.py
@@ -0,0 +1,5 @@
+cubes = []
+for number in range(1,10):
+    cube = number **3
+    cubes.append(cube)
+print(cubes)
