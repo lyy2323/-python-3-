4ae3062ecbdf66199f7fcaf21c05f835cedc4c20
lyf85 <lyf8511@outlook.com>
2024-12-21 08:16:35 +0800

    4

--- P编程-P1-基础部分/04操作列表/04使用列表的一部分.py
+++ P编程-P1-基础部分/04操作列表/04使用列表的一部分.py
@@ -1,6 +1,9 @@
-players = ['111', '222', '333', '444', '555']
+players = ['one', 'tow', '333', '444', '555']
 print(players[1:3])
 print(players[:3])
 print(players[-3:])
 print(players[:-3])
 print(players[1:5:2]) # 其中2是步长，注意切片之间是冒号，不仅位置，也包括步长之间
+# 通过for循环使用切片，显示位置名称：
+for number in players[1:3]:
+    print(number.title())
