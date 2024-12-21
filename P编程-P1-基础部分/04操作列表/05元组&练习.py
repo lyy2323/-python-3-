124ba12776c0efae508918a734c101a2f2830116
lyf85 <lyf8511@outlook.com>
2024-12-21 15:43:39 +0800

    4

---
+++ P编程-P1-基础部分/04操作列表/05元组&练习.py
@@ -0,0 +1,13 @@
+# 建立5个元组，用for循环打印
+restaurant = ('rice', 'noodle' 'pasta', 'pizza', 'pineapple')
+for food in restaurant:
+    print(food)
+# 尝试修改元组（下面的代码是列表，如果将其改为元组形式，即第一行的方括号改为圆括号，即报错）：
+restaurants = ["111", '111114', '113232']
+restaurants[2] = '2111'
+print(restaurants)
+
+# 更换第一行的元组内容，然后用for循环打印第一段的元组
+restaurant = ('rice', 'noodle', 'dumpling', 'chick')
+for food in restaurant:
+    print(food)
